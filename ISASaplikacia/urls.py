9ffb69eb46d1cf6ff8d2a8c54f712f984288fa81
Pavol Slabý <pavolslaby44@gmail.com>
2025-09-25 20:22:37 +0200

    Add home view to urls.py

--- ISASaplikacia/urls.py
+++ ISASaplikacia/urls.py
@@ -14,21 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
 
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("✅ Vitaj v ISAS aplikácii! Server beží správne 🚀")
+    return HttpResponse("✅ Vitajte v ISAS aplikácii! Server beží správne 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', home, name='home'),
 ]
